7bfe99d288cd07b636083616b46800d5d912f388
GuruRK <gururamkoushik@gmail.com>
2018-11-14 00:05:36 +0530

    executed stack cards

--- App/App.js
+++ App/App.js
@@ -1,60 +1,32 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Card, Button } from "react-native-elements";
-import Carddeck from "./src/Card-deck";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Card, Button } from 'react-native-elements';
+import Carddeck from './src/Card-deck'
 
 const DATA = [
-  {
-    id: 1,
-    text: "Card #1",
-    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg"
-  },
-  {
-    id: 2,
-    text: "Card #2",
-    uri: "http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg"
-  },
-  {
-    id: 3,
-    text: "Card #3",
-    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg"
-  },
-  {
-    id: 4,
-    text: "Card #4",
-    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg"
-  },
-  {
-    id: 5,
-    text: "Card #5",
-    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg"
-  },
-  {
-    id: 6,
-    text: "Card #6",
-    uri: "http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg"
-  },
-  {
-    id: 7,
-    text: "Card #7",
-    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg"
-  },
-  {
-    id: 8,
-    text: "Card #8",
-    uri: "http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg"
-  }
+  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+  { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
 ];
 
 export default class App extends React.Component {
   renderCard(item) {
     return (
-      <Card key={item.id} title={item.text} image={{ uri: item.uri }}>
+      <Card
+        key={item.id}
+        title={item.text}
+        image={{ uri: item.uri }}
+      >
         <Text style={{ marginBottom: 10 }}>
           I can customize the Card further.
         </Text>
         <Button
-          icon={{ name: "code" }}
+          icon={{ name: 'code' }}
           backgroundColor="#03A9F4"
           title="View Now!"
         />
@@ -64,29 +36,37 @@ export default class App extends React.Component {
 
   renderNoMoreCards() {
     return (
-      <Card title="That's It For Now">
-        <Text style={{ marginBottom: 10 }}>
+      <Card title = "That's It For Now">
+        <Text style = {{ marginBottom: 10 }}>
           There's No More Information For Now!
         </Text>
-        <Button style={{ backgroundColor: "black" }} title="Get More! " />
+        <Button
+        style = {{backgroundColor : 'black'}}
+        title = "Get More!">
+        </Button>
       </Card>
-    );
+    )
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Carddeck data={DATA} renderCard={this.renderCard}
-        renderNoMoreCards = {this.renderNoMoreCards} />
+      <View style = {styles.container}>
+        <Carddeck 
+          data = {DATA}  
+          renderCard = {this.renderCard}
+          renderNoMoreCards = {this.renderNoMoreCards}
+        />
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 20,
-    flex: 1,
-    backgroundColor: "#fff"
-  }
-});
+const styles = StyleSheet.create ({
+   container : {
+     marginTop : 20,
+     flex : 1,
+     backgroundColor : '#fff'
+   }
+})
+
+
